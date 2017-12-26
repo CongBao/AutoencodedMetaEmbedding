c7cc7cd8b16cfc1e1f9571b783dbc6f24681bdb5
Cong Bao <bao_cong@outlook.com>
2017-12-26 12:10:09 +0000

    init environment

--- aeme/scripts/run.sh
+++ aeme/scripts/run.sh
@@ -4,5 +4,8 @@ IN="~/data/CBOW-full.txt ~/data/GloVe-full.txt"
 OUT="~/new_results/ae_SP_5.txt"
 MODEL="ae AEModel"
 
+`export PATH=$PATH:/usr/local/cuda/bin`
+`export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib:/usr/local/cuda/lib64`
+
 `rm nohup.out`
 `nohup python ../execute.py --module-path ~/aeme -m $MODEL -i $IN -o $OUT -b 128 -e 1000 --noise-type SP --noise-ratio 0.05 &`

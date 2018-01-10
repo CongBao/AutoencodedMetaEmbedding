146c7c68fc3dff81bbcf835f6788ea97ad08d92c
Cong Bao <bao_cong@outlook.com>
2018-01-10 21:52:05 +0000

    update normalization

--- aeme/utils/preprocess.py
+++ aeme/utils/preprocess.py
@@ -7,10 +7,16 @@ import sklearn.preprocessing as skpre
 
 __author__ = 'Cong Bao'
 
+def normalize(vector, scale_factor):
+    """
+    normalize a vector
+    """
+    return scale_factor * skpre.normalize(vector.reshape(1, -1))[0]
+
 def normalize_embeddings(embedding_dict, scale_factor):
     """
     normalize the embeddings
     """
     for word, values in embedding_dict.items():
-        embedding_dict[word] = scale_factor * skpre.normalize(values.reshape(1, -1))[0]
+        embedding_dict[word] = normalize(values, scale_factor)
     return embedding_dict

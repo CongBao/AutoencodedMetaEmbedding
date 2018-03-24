a0af8b7d364126ae94ce01cb14ea543ddd9cad08
Cong Bao <bao_cong@outlook.com>
2018-03-24 11:02:03 +0000

    add pickout

---
+++ aeme/pickout.py
@@ -0,0 +1,21 @@
+# pick out intersection of source embedding
+
+from __future__ import print_function
+
+import argparse
+
+import numpy as np
+import sklearn.preprocessing as skpre
+
+from utils import load_emb, save_emb
+
+__author__ = 'Cong Bao'
+
+def pickout(inputs, output):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()

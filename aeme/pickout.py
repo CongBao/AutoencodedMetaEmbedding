058d7b5077a63293cb0544987a5f434895dd930b
Cong Bao <bao_cong@outlook.com>
2018-03-24 11:41:10 +0000

    add pickout

--- aeme/pickout.py
+++ aeme/pickout.py
@@ -7,15 +7,33 @@ import argparse
 import numpy as np
 import sklearn.preprocessing as skpre
 
-from utils import load_emb, save_emb
+from utils import Utils
 
 __author__ = 'Cong Bao'
 
-def pickout(inputs, output):
-    pass
+def pickout(inputs, outputs):
+    utils = Utils()
+    src_dict_list = [utils.load_emb(path) for path in inputs]
+    inter_words = list(set.intersection(*[set(src_dict.keys()) for src_dict in src_dict_list]))
+    print('Intersection Words: %s' % len(inter_words))
+    normalize = lambda x: skpre.normalize(np.reshape(x, (1, -1)))[0]
+    for i, path in enumerate(outputs):
+        selected = {}
+        for word in inter_words:
+            selected[word] = normalize(src_dict_list[i][word])
+        utils.save_emb(selected, path)
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    add_arg = parser.add_argument
+    add_arg('-i', dest='input',  nargs='+', type=str, required=True, help='input files')
+    add_arg('-o', dest='output', nargs='+', type=str, required=True, help='output files')
+    args = parser.parse_args()
+    assert len(args.input) == len(args.output)
+    print('Input files: %s' %  args.input)
+    print('Output files: %s' % args.output)
+    pickout(args.input, args.output)
+    print('Complete')
 
 if __name__ == '__main__':
     main()

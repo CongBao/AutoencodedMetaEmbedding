39aa70e50856372b025888ccecfbfa94191ec807
CongBao <bao_cong@outlook.com>
2018-08-14 10:33:55 +0800

    add k-means

---
+++ aeme/utils/cluster.py
@@ -0,0 +1,46 @@
+# K-means Cluster
+# File: cluster.py
+#Author: Cong Bao
+
+from __future__ import print_function
+
+import argparse
+
+import numpy as np
+from sklearn.cluster import KMeans
+
+from utils import Utils
+
+__author__ = 'Cong Bao'
+
+def cluster(input_path, pred_word, n):
+    util = Utils()
+    emb_dict = util.load_emb(input_path)
+    test = emb_dict.pop(pred_word)
+    words = []
+    embds = []
+    for word, emb in emb_dict.items():
+        words.append(word)
+        embds.append(emb)
+    del emb_dict
+    kmeans = KMeans(n_clusters=n).fit(embds)
+    print("[Data Labels]")
+    for w, l in zip(words, kmeans.labels_):
+        print("%s: %d" % (w, l))
+    res = kmeans.predict([test])
+    print("[Predict Result] %s: %d" % (pred_word, res[0]))
+
+def main():
+    parser = argparse.ArgumentParser()
+    add_arg = parser.add_argument
+    add_arg('-i', dest='input', type=str, required=True, help='embedding path')
+    add_arg('-w', dest='word', type=str, required=True, help='word to predict')
+    add_arg('-n', dest='num', type=int, default=2, help='number of clusters')
+    args = parser.parse_args()
+    try:
+        cluster(args.input, args.word, args.num)
+    except (KeyboardInterrupt, SystemExit):
+        print('Abort!')
+
+if __name__ == '__main__':
+    main()
--- aeme/utils/visualize.py
+++ aeme/utils/visualize.py
@@ -8,13 +8,14 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
 
 from utils import Utils
 
 __author__ = 'Cong Bao'
 
-def visualize(input_path, output_path, fig_size=(8, 8)):
+def visualize(input_path, output_path, fig_size=(8, 8), cluster=False, p_word=None):
     """ Visualize the word embeddings.
         :param input_path: path of word embedding file
         :param output_path: path of output image
@@ -28,17 +29,25 @@ def visualize(input_path, output_path, fig_size=(8, 8)):
         labels.append(word)
         tokens.append(embed)
     del emb_dict
-    tsne = TSNE(perplexity=40, learning_rate=100, n_iter=5000, n_iter_without_progress=5000, init='pca', verbose=2)
+    tsne = TSNE(perplexity=40, learning_rate=100, init='pca', verbose=2)
     zipped = tsne.fit_transform(tokens)
-    del tokens
     x = []
     y = []
     for embed in zipped:
         x.append(embed[0])
         y.append(embed[1])
+    if cluster:
+        kmeans = KMeans(n_clusters=2).fit(tokens)
     plt.figure(figsize=tuple(fig_size))
     for i in range(len(x)):
-        plt.scatter(x[i], y[i])
+        if cluster:
+            color = 'red' if kmeans.labels_[i] else 'blue'
+            if p_word and i in [labels.index(p) for p in p_word]:
+                plt.scatter(x[i], y[i], c='green')
+            else:
+                plt.scatter(x[i], y[i], c=color)
+        else:
+            plt.scatter(x[i], y[i])
         plt.annotate(labels[i], xy=(x[i], y[i]), xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')
     print('Saving figure...')
     plt.savefig(output_path)
@@ -51,9 +60,11 @@ def main():
     add_arg('-i', dest='input', type=str, required=True, help='embedding path')
     add_arg('-o', dest='output', type=str, required=True, help='figure saving path')
     add_arg('-s', dest='size', type=int, nargs='+', default=[8, 8], help='the size of figure in inches')
+    add_arg('-C', dest='cluster', action='store_true', help='perform k-means cluster')
+    add_arg('-W', dest='word', type=str, nargs='+', default=None, help='pickout word')
     args = parser.parse_args()
     try:
-        visualize(args.input, args.output, args.size)
+        visualize(args.input, args.output, args.size, args.cluster, args.word)
     except (KeyboardInterrupt, SystemExit):
         print('Abort!')
 

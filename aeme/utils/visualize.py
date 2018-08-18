612b560233ce7f38c9b89a89aac6d328e31a260d
CongBao <bao_cong@outlook.com>
2018-08-18 10:06:13 +0800

    update visualize

--- aeme/utils/visualize.py
+++ aeme/utils/visualize.py
@@ -9,13 +9,14 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
 from utils import Utils
 
 __author__ = 'Cong Bao'
 
-def visualize(input_path, output_path, fig_size=(8, 8), cluster=False, p_word=None):
+def visualize(input_path, output_path, fig_size=(8, 8), cluster=0, p_word=None, algo='PCA'):
     """ Visualize the word embeddings.
         :param input_path: path of word embedding file
         :param output_path: path of output image
@@ -29,21 +30,34 @@ def visualize(input_path, output_path, fig_size=(8, 8), cluster=False, p_word=No
         labels.append(word)
         tokens.append(embed)
     del emb_dict
-    tsne = TSNE(perplexity=40, learning_rate=100, init='pca', verbose=2)
-    zipped = tsne.fit_transform(tokens)
+    if algo == 'PCA':
+        pca = PCA(n_components=2)
+        zipped = pca.fit_transform(tokens)
+    elif algo == 'TSNE':
+        tsne = TSNE(perplexity=40, learning_rate=100, init='pca', verbose=2)
+        zipped = tsne.fit_transform(tokens)
+    else:
+        return
     x = []
     y = []
     for embed in zipped:
         x.append(embed[0])
         y.append(embed[1])
     if cluster:
-        kmeans = KMeans(n_clusters=2).fit(tokens)
+        kmeans = KMeans(n_clusters=cluster).fit(tokens)
     plt.figure(figsize=tuple(fig_size))
     for i in range(len(x)):
         if cluster:
-            color = 'red' if kmeans.labels_[i] else 'blue'
+            if kmeans.labels_[i] == 0:
+                color = 'red'
+            elif kmeans.labels_[i] == 1:
+                color = 'blue'
+            elif kmeans.labels_[i] == 2:
+                color = 'green'
+            else:
+                color = 'black'
             if p_word and i in [labels.index(p) for p in p_word]:
-                plt.scatter(x[i], y[i], c='green')
+                plt.scatter(x[i], y[i], c='black')
             else:
                 plt.scatter(x[i], y[i], c=color)
         else:
@@ -60,11 +74,12 @@ def main():
     add_arg('-i', dest='input', type=str, required=True, help='embedding path')
     add_arg('-o', dest='output', type=str, required=True, help='figure saving path')
     add_arg('-s', dest='size', type=int, nargs='+', default=[8, 8], help='the size of figure in inches')
-    add_arg('-C', dest='cluster', action='store_true', help='perform k-means cluster')
+    add_arg('-C', dest='cluster', type=int, default=0, help='perform k-means cluster')
     add_arg('-W', dest='word', type=str, nargs='+', default=None, help='pickout word')
+    add_arg('-A', dest='algo', type=str, nargs='+', default='PCA', help='algorithm used to reduce dim')
     args = parser.parse_args()
     try:
-        visualize(args.input, args.output, args.size, args.cluster, args.word)
+        visualize(args.input, args.output, args.size, args.cluster, args.word, args.algo)
     except (KeyboardInterrupt, SystemExit):
         print('Abort!')
 
